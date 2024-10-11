9ba34297ca583e60cd8aa628f7c50aef809c3080
Victor Fernandez <fernandezvictor738@gmail.com>
2024-10-11 18:59:34 +0200

    Conflicto resuelto

--- convert.js
+++ convert.js
@@ -2,7 +2,7 @@ function convertirCF() {
     let CF = prompt("A què vols convertir C/F?");
     let temp = parseFloat(prompt("Temperatura:"));
     if( CF.toUpperCase() == "F" ){
-        let cent = (temp-32)/(9/5);
+        let cent = (temp-32)/(5/9);
         console.log(`${temp}F -> ${cent}C`);
     }else if (CF.toUpperCase() == "C"){
         let fh = (9/5) * temp + 32;
