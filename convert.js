fdba6de603a6c86bdc963bd0fd7c4e2ee0fddbd2
Victor Fernandez <fernandezvictor738@gmail.com>
2024-10-11 18:35:48 +0200

    Canvis a convert.js

--- convert.js
+++ convert.js
@@ -1,12 +1,12 @@
 function convertirCF() {
     let CF = prompt("A què vols convertir C/F?");
     let temp = parseFloat(prompt("Temperatura:"));
-    if( CF.toUpperCase() == "C" ){
-        let cent = (temp-32)/(5/9);
+    if( CF.toUpperCase() == "F" ){
+        let cent = (temp-32)/(9/5);
         console.log(`${temp}F -> ${cent}C`);
-    }else if (CF.toUpperCase() == "F"){
-        let fh = (5/9) * temp + 32;
-        console.log(`${temp}C -> ${fh}F`);
+    }else if (CF.toUpperCase() == "C"){
+        let fh = (9/9) * temp + 32;
+        console.log(`${temp} C -> ${fh}F`);
     }else {
         console.log("Només es pot C ó F");
     }
