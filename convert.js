6517e7a38841e3bd397396ce02cf5ad37d40253f
Victor Fernandez <fernandezvictor738@gmail.com>
2024-10-11 18:53:57 +0200

    canvis .js

--- convert.js
+++ convert.js
@@ -5,8 +5,8 @@ function convertirCF() {
         let cent = (temp-32)/(9/5);
         console.log(`${temp}F -> ${cent}C`);
     }else if (CF.toUpperCase() == "C"){
-        let fh = (9/9) * temp + 32;
-        console.log(`${temp} C -> ${fh}F`);
+        let fh = (9/5) * temp + 32;
+        console.log(`${temp}C -> ${fh}F`);
     }else {
         console.log("Només es pot C ó F");
     }
